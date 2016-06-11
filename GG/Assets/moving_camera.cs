ca9de6204bc3f6ea2e86dc96e0a7f639d76337bb
julijan040 <js1840@student.uni-lj.si>
2016-06-11 12:59:48 +0200

    dd

---
+++ GG/Assets/moving_camera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moving_camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+
+    }
+}
